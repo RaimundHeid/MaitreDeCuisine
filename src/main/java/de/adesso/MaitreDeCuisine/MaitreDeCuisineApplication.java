916417035d7c8af5dfb0925b17cceacac2442e8a
Raimund Heid <Raimund_Heid@gmx.de>
2018-06-14 19:28:17 +0200

    init from https://start.spring.io/

---
+++ src/main/java/de/adesso/MaitreDeCuisine/MaitreDeCuisineApplication.java
@@ -0,0 +1,12 @@
+package de.adesso.MaitreDeCuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaitreDeCuisineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaitreDeCuisineApplication.class, args);
+	}
+}
