df5f2429727bc0933a29141a826f21f20491a03c
Raimund Heid <Raimund_Heid@gmx.de>
2018-06-14 23:05:34 +0200

    Fix

--- src/main/java/de/adesso/MaitreDeCuisine/ctrl/KitchenController.java
+++ src/main/java/de/adesso/MaitreDeCuisine/ctrl/KitchenController.java
@@ -3,15 +3,14 @@ package de.adesso.MaitreDeCuisine.ctrl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 public class KitchenController {
 
     @GetMapping("/recipes/{ingredients}")
-    public ResponseEntity<String> getReceipes(@PathParam(value = "ingredients") String ingredients) {
+    public ResponseEntity<String> getReceipes(@PathVariable(value = "ingredients") String ingredients) {
         ResponseEntity<String> response = new ResponseEntity<>("So you own " + ingredients + "?", HttpStatus.OK);
 
         return response;
